fcb674afad38c706f27540337db7976e1c991d34
Vap3er <103359678+Vap3er@users.noreply.github.com>
2024-11-27 21:35:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const request = require('request');
+const http = require('http');
+const https = require('https');
 const app = express();
 
-const proxyURL = 'http://212.62.95.45:8080'; // Hier kannst du deinen Proxy-Server ändern
-
 app.use((req, res) => {
-  const url = req.url.substring(1); // Entfernt das führende "/" aus der URL
-  const options = {
-    url: url,
-    proxy: proxyURL,
-  };
+  const targetUrl = req.url.substring(1); // Entfernt das führende "/" aus der URL
+  if (!targetUrl) {
+    return res.status(400).send('Bad Request: Target URL not specified');
+  }
 
-  request(options)
-    .on('error', (err) => {
-      console.error('Proxy Error:', err);
-      res.status(500).send('Proxy Error');
-    })
-    .pipe(res);
+  const protocol = targetUrl.startsWith('https') ? https : http;
+  protocol.get(targetUrl, (proxyRes) => {
+    proxyRes.pipe(res, { end: true });
+  }).on('error', (err) => {
+    console.error('Proxy Error:', err);
+    res.status(500).send('Proxy Error');
+  });
 });
 
 const PORT = process.env.PORT || 3000;
